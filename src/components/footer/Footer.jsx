611ad686f8a32b37bb1e0992b679461d44cd2e0b
UmidaJamshidbekovna <78403650+UmidaJamshidbekovna@users.noreply.github.com>
2022-10-20 06:58:44 +0500

    newOne

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './footer.css'
 
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsTelegram} from 'react-icons/bs'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <a href="#" className='footer__logo'></a>
+
+        <ul className='permalinks'>
+            <li>
+              <a href="#">Home</a>
+            </li>
+
+            <li>
+              <a href="#about">About</a>
+            </li>
+
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+        </ul>
+
+        <div className="footer__socials">
+
+          <a href="https://linkedin.com/in/umida-rakhimova-08b037222" target='_blank'> <BsLinkedin/> </a>
+          <a href="https://github.com/UmidaJamshidbekovna" target='_blank'> <BsGithub/> </a>
+         <a href="https://t.me/XCBsU" target='_blank'> <BsTelegram/> </a>
+
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; Rakhimova Umida Profile.    All right reserved.</small>
+        </div>
+    </footer>
   )
 }
 
